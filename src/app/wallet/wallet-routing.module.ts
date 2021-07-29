7fde1fb25cfca0987a3f24d2b97ede860a947d59
Josué Azevedo <josue-azevedo@hotmail.com>
2021-07-29 09:21:20 -0300

    feat: create componente wallet-dashboard

---
+++ src/app/wallet/wallet-routing.module.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { WalletDashboardComponent } from './wallet-dashboard/wallet-dashboard.component';
+
+export const WalletRouting: Routes = [
+  {
+    path: '',
+    component: WalletDashboardComponent
+  }
+];
--- src/app/wallet/wallet.module.ts
+++ src/app/wallet/wallet.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { WalletDashboardComponent } from './wallet-dashboard/wallet-dashboard.component';
+import { RouterModule } from '@angular/router';
+import { WalletRouting } from './wallet-routing.module';
+import { WalletNavbarComponent } from './components/wallet-navbar/wallet-navbar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    WalletDashboardComponent,
+    WalletNavbarComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(WalletRouting),
   ]
 })
 export class WalletModule { }
