24adf78b13d930f3256f9176808ba117fc7f966a
Josué Azevedo <josue-azevedo@hotmail.com>
2021-07-29 09:35:42 -0300

    feat: get balance request

--- src/app/wallet/wallet.module.ts
+++ src/app/wallet/wallet.module.ts
@@ -5,8 +5,10 @@ import { RouterModule } from '@angular/router';
 import { WalletRouting } from './wallet-routing.module';
 import { WalletNavbarComponent } from './components/wallet-navbar/wallet-navbar.component';
 import { BalanceComponent } from './components/balance/balance.component';
+import { IConfig, NgxMaskModule } from 'ngx-mask';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-
+export const options: Partial<IConfig> | (() => Partial<IConfig>) = null;
 
 @NgModule({
   declarations: [
@@ -17,6 +19,9 @@ import { BalanceComponent } from './components/balance/balance.component';
   imports: [
     CommonModule,
     RouterModule.forChild(WalletRouting),
+    NgxMaskModule.forRoot(),
+    FormsModule,
+    ReactiveFormsModule,
   ]
 })
 export class WalletModule { }
