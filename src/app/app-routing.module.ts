ab03fa89ca5a01c2f0459c9631e1add50bb36c85
Josué Azevedo <josue-azevedo@hotmail.com>
2021-07-29 09:17:40 -0300

    feat: configure router module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>  import('src/app/wallet/wallet.module').then(m => m.WalletModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
