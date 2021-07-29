3174fbd0e27c60f2f1d8baeca51d2ba3591a6070
Josué Azevedo <josue-azevedo@hotmail.com>
2021-07-29 15:06:32 -0300

    feat: create category component

---
+++ src/app/wallet/categories/categories.component.ts
@@ -0,0 +1,87 @@
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { ToastrService } from 'ngx-toastr';
+import { CategoryService } from '../category.service';
+import { Category } from '../models/category.model';
+
+@Component({
+  selector: 'app-categories',
+  templateUrl: './categories.component.html',
+  styleUrls: ['./categories.component.css']
+})
+export class CategoriesComponent implements OnInit {
+
+  listCategories: any = []
+  loading = false
+  modalRef: BsModalRef;
+  name = new FormControl('', [Validators.required]);
+  category: Category
+
+  constructor(
+    private toastr: ToastrService,
+    private categoryService: CategoryService,
+    private modalService: BsModalService
+  ) { }
+
+  ngOnInit(): void {
+    this.getCategories()
+  }
+
+  openMocalCreateCategory(template: TemplateRef<any>){
+    this.category = new Category()
+    this.openModal(template)
+  }
+  openMocalEditCategory(template: TemplateRef<any>, category: Category){
+    this.category = new Category()
+    this.category.id = category.id
+    this.category.name = category.name
+    this.openModal(template)
+  }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
+
+  async getCategories() {
+    this.loading = true
+    try {
+      const result = await this.categoryService.getCategories().toPromise()
+      this.listCategories = result
+    } catch (e) {
+      this.toastr.error('Tivemos um problema, tente novamente')
+    }
+    this.loading = false
+  }
+
+  async saveCategory() {
+    this.loading = true
+    try {
+      const result = await this.categoryService.createCategory(this.category).toPromise()
+      this.listCategories.push(result)
+      this.modalService.hide()
+      this.toastr.success('Salvo com sucesso')
+    } catch (e) {
+      this.toastr.error('Tivemos um problema, tente novamente')
+    }
+    this.loading = false
+  }
+
+  async updateCategory(category: Category) {
+    this.loading = true
+    try {
+      const result = await this.categoryService.updateCategory(this.category).toPromise()
+      this.listCategories.map((item, i) => {
+        if (item.id === result['id']){
+          this.listCategories[i] = result
+        }
+      }) 
+      this.modalService.hide()
+      this.toastr.success('Salvo com sucesso')
+    } catch (e) {
+      this.toastr.error('Tivemos um problema, tente novamente')
+    }
+    this.loading = false
+  }
+
+}
---
+++ src/app/wallet/category.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Category } from './models/category.model';
+
+const BASE_URL = 'http://localhost:4000/'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  constructor(private http: HttpClient) { }
+
+  getCategories() {
+    return this.http.get(BASE_URL + 'category')
+  }
+
+  createCategory(category: Category) {
+    return this.http.post(BASE_URL + 'category', category)
+  }
+
+  updateCategory(category: Category) {
+    return this.http.put(BASE_URL + 'category', category)
+  }
+
+}
